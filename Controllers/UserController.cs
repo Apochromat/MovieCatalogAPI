970f3d76651fb06d9c96fd946eb7656eb802146f
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-10-23 12:29:11 +0700

    feat(user): добавил в UserController атрибуты проверки актуальности токена

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,6 +8,7 @@ using webNET_Hits_backend_aspnet_project_1.Models.DTO;
 using webNET_Hits_backend_aspnet_project_1.Models.Enum;
 using webNET_Hits_backend_aspnet_project_1.Services;
 using System.Security.Principal;
+using System.Diagnostics;
 
 namespace webNET_Hits_backend_aspnet_project_1.Controllers
 {
@@ -16,27 +17,29 @@ namespace webNET_Hits_backend_aspnet_project_1.Controllers
     public class UserController : ControllerBase {
         private readonly ApplicationDbContext db;
         private readonly IUserService _userService;
+        private readonly ICacheService _cacheService;
+        private readonly ILogger<UserController> _logger;
 
-        public UserController(ApplicationDbContext context, IUserService userService) {
+        public UserController(ApplicationDbContext context, IUserService userService, ICacheService cacheService, ILogger<UserController> logger) {
             db = context;
             _userService = userService;
-        }
+            _cacheService = cacheService;
+            _logger = logger;
+        }  
 
         [Authorize]
         [HttpGet]
         [Route("profile")]
-        public IActionResult Get() {
-            if (!ModelState.IsValid) { 
-                return BadRequest(ModelState);
-            }
-
+        public async Task<IActionResult> GetAsync() {
             try {
+                if (await _cacheService.IsTokenDead(Request.Headers["Authorization"])) return Unauthorized("Token is expired");
                 String username = User.Identity.Name;
                 ProfileModel profileModel = _userService.getprofile(username, db);
                 return Ok(profileModel);
             } catch (ArgumentNullException e) {
                 return Problem(statusCode: 404, title: e.Message);
-            } catch (Exception) {
+            } catch (Exception e) {
+                _logger.LogError(e, e.Message);
                 return Problem(statusCode: 500, title: "Something went wrong");
             }
         }
@@ -48,14 +51,15 @@ namespace webNET_Hits_backend_aspnet_project_1.Controllers
             if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
-
             try {
+                if (await _cacheService.IsTokenDead(Request.Headers["Authorization"])) return Unauthorized("Token is expired");
                 String username = User.Identity.Name;
                 await _userService.modifyprofile(username, profileModel, db);
                 return Ok();
             } catch (ArgumentNullException e) {
                 return NotFound(e.Message);
-            } catch (Exception) {
+            } catch (Exception e) {
+                _logger.LogError(e, e.Message);
                 return Problem(statusCode: 500, title: "Something went wrong");
             }
         }
