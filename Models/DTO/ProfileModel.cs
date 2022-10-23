f6be27034c315a8fba7e97f3eb8fb59b255c19be
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-10-23 16:16:30 +0700

    fix(models): добавил атрибуты валидации в моделях LoginCredentials, ProfileModel, UserRegsterModel

--- Models/DTO/ProfileModel.cs
+++ Models/DTO/ProfileModel.cs
@@ -3,22 +3,33 @@ using webNET_Hits_backend_aspnet_project_1.Models.Enum;
 
 namespace webNET_Hits_backend_aspnet_project_1.Models.DTO {
     public class ProfileModel {
+        [Required]
         public Guid id { get; set; }
 
-        public String? nickName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9\-_]+$")]
+        [MinLength(2)]
+        [MaxLength(32)]
+        public String nickName { get; set; }
 
         [Required]
-        [EmailAddress(ErrorMessage = "Некорректный email-адрес")]
+        [EmailAddress]
         public String email { get; set; }
 
+        [Url]
         public String? avatarLink { get; set; }
 
         [Required]
+        [RegularExpression(@"^[а-яА-Яa-zA-Z0-9\-_ ]+$")]
+        [MinLength(4)]
+        [MaxLength(64)]
         public String name { get; set; }
 
         [Required]
+        [DataType(DataType.DateTime)]
         public DateTime birthDate { get; set; }
 
+        [Required]
         public Gender gender { get; set; }
     }
 }
--- Models/DTO/UserRegisterModel.cs
+++ Models/DTO/UserRegisterModel.cs
@@ -4,12 +4,21 @@ using webNET_Hits_backend_aspnet_project_1.Models.Enum;
 namespace webNET_Hits_backend_aspnet_project_1.Models.DTO {
     public class UserRegisterModel {
         [Required]
+        [RegularExpression(@"^[a-zA-Z0-9\-_]+$")]
+        [MinLength(2)]
+        [MaxLength(32)]
         public String userName { get; set; }
 
         [Required]
+        [RegularExpression(@"^[а-яА-Яa-zA-Z0-9\-_ ]+$")]
+        [MinLength(4)]
+        [MaxLength(64)]
         public String name { get; set; }
 
         [Required]
+        [RegularExpression(@"^[a-zA-Z0-9\-_!@#№$%^&?*+=(){}[\]<>~]+$")]
+        [MinLength(8)]
+        [MaxLength(64)]
         public String password { get; set; }
 
         [EmailAddress]
@@ -17,6 +26,7 @@ namespace webNET_Hits_backend_aspnet_project_1.Models.DTO {
         public String email { get; set; }
 
         [Required]
+        [DataType(DataType.DateTime)]
         public DateTime birthDate { get; set; }
 
         [Required]
