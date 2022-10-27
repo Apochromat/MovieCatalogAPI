446e428f6615e6f28b44156b30eccf0cfc0b2436
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-10-27 20:35:52 +0700

    feat(favorites): реализовал сервис IFavoriteMoviesService

--- Controllers/FavoriteMoviesController.cs
+++ Controllers/FavoriteMoviesController.cs
@@ -2,12 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 using webNET_Hits_backend_aspnet_project_1.Models;
 using webNET_Hits_backend_aspnet_project_1.Models.DTO;
 using webNET_Hits_backend_aspnet_project_1.Models.Enum;
+using webNET_Hits_backend_aspnet_project_1.Services;
 
 namespace webNET_Hits_backend_aspnet_project_1.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("/api/favorites")]
     public class FavoriteMoviesController : ControllerBase {
+        private readonly ApplicationDbContext db;
+        private readonly IFavoriteMoviesService _favoriteMoviesService;
+        private readonly ICacheService _cacheService;
+        private readonly ILogger<FavoriteMoviesController> _logger;
 
+        public FavoriteMoviesController(ApplicationDbContext context, IFavoriteMoviesService favoriteMoviesService, ICacheService cacheService, ILogger<FavoriteMoviesController> logger) {
+            db = context;
+            _favoriteMoviesService = favoriteMoviesService;
+            _cacheService = cacheService;
+            _logger = logger;
+        }
     }
 }
