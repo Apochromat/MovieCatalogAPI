e171bfb252a795e73d06ce35448d64cb579ee065
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-10-27 13:41:29 +0700

    feat(review): добавил драфт ReviewService

---
+++ Services/IReviewService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using webNET_Hits_backend_aspnet_project_1.Models.DTO;
+
+namespace webNET_Hits_backend_aspnet_project_1.Services {
+    public interface IReviewService {
+        Task<IActionResult> addreview(String Username, ReviewModifyModel reviewModifyModel, ApplicationDbContext db);
+
+        Task<IActionResult> editreview(Guid ReviewId, ReviewModifyModel reviewModifyModel, ApplicationDbContext db);
+
+        Task<IActionResult> deletereview(Guid ReviewId, ApplicationDbContext db);
+
+        List<ReviewModel> getreviewmodels(Guid MovieId, ApplicationDbContext db);
+
+        List<ReviewShortModel> getreviewshortmodels(Guid MovieId, ApplicationDbContext db);
+    }
+}
