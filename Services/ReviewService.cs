81b83019a2a2179e5d7a82225602cd0d47958469
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-10-27 18:24:03 +0700

    feat(review): реализовал метод сервиса addreview

--- Services/ReviewService.cs
+++ Services/ReviewService.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webNET_Hits_backend_aspnet_project_1.Models;
 using webNET_Hits_backend_aspnet_project_1.Models.DTO;
 
 namespace webNET_Hits_backend_aspnet_project_1.Services {
     public class ReviewService : IReviewService {
-        public Task<IActionResult> addreview(string Username, ReviewModifyModel reviewModifyModel, ApplicationDbContext db) {
-            throw new NotImplementedException();
+        public async Task<IActionResult> addreview(string Username, ReviewModifyModel reviewModifyModel, ApplicationDbContext db) {
+            User? user = db.Users.Where(x => x.Username == Username).FirstOrDefault();
+            if (user == null) { throw new KeyNotFoundException(); }
+            Review review = new Review(reviewModifyModel, user);
+
+            await db.Reviews.AddAsync(review);
+            await db.SaveChangesAsync();
+            return new OkResult();
         }
 
         public Task<IActionResult> deletereview(Guid ReviewId, ApplicationDbContext db) {
