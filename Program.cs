42f04c325710c3fa468d60d759e1315c7ca52129
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-10-27 20:14:25 +0700

    feat(review): реализовал все endpoints

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ builder.Services.AddScoped<IGenreService, GenreService>();
 builder.Services.AddSingleton<IDistributedCache, RedisCache>();
 builder.Services.AddScoped<ICacheService, CacheService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
 
 // Add DbContext
 builder.Services.AddDbContext<ApplicationDbContext>();
