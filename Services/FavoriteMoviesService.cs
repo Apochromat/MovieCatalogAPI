0c2d20fc3bc2764151059be5f0d8284cc211fff6
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-10-27 13:47:06 +0700

    feat(favorites): добавил драфт FavoriteMoviesService

---
+++ Services/FavoriteMoviesService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webNET_Hits_backend_aspnet_project_1.Models.DTO;
+
+namespace webNET_Hits_backend_aspnet_project_1.Services {
+    public class FavoriteMoviesService : IFavoriteMoviesService {
+        public Task<IActionResult> addfavorites(string Username, Guid MovieId, ApplicationDbContext db) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> deletefavorites(string Username, Guid MovieId, ApplicationDbContext db) {
+            throw new NotImplementedException();
+        }
+
+        public MoviesListModel getfavorites(string Username, ApplicationDbContext db) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IFavoriteMoviesService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using webNET_Hits_backend_aspnet_project_1.Models.DTO;
+
+namespace webNET_Hits_backend_aspnet_project_1.Services {
+    public interface IFavoriteMoviesService {
+        MoviesListModel getfavorites(String Username, ApplicationDbContext db);
+
+        Task<IActionResult> addfavorites(String Username, Guid MovieId, ApplicationDbContext db);
+
+        Task<IActionResult> deletefavorites(String Username, Guid MovieId, ApplicationDbContext db);
+    }
+}
